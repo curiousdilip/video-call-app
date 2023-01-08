ef6a2a88efbb7794ea668c71a36e491d473868d2
Dilip Kumar <curiousdilip@gmail.com>
2023-01-08 10:54:43 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import JoinRoom from './JoinRoom'
+import Room from './Room';
+import './styles.css'
+
+function App() {
+
+  return (
+    <div className="App wrapper"> 
+      <JoinRoom />
+    </div>
+  );
+}
+
+export default App;
